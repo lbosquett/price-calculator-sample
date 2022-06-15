de23c7665612ee2c8317b210bc7f28895718ea5f
Leonardo Bosquett <46539053+lbosquett@users.noreply.github.com>
2022-06-15 20:24:24 -0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Calculator from "./components/calculator.vue";
+import AppHeader from "./components/layout/app-header.vue";
+</script>
+
+<template>
+  <div class="header-container">
+    <div class="page-800">
+      <app-header />
+    </div>
+  </div>
+  <div class="page-800">
+    <calculator />
+  </div>
+</template>
+
+<style scoped>
+.page-800 {
+  width: clamp(600px, 800px, 100%);
+  margin: 0 auto;
+}
+
+.header-container {
+  height:100px;
+  border-bottom: 1px solid #f4f4f4;
+  margin-bottom: 20px;
+}
+</style>
